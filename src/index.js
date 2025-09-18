b1d6ee027092054a0aa65b306701a60f141f36e0
허쌤 <withmyload@gmail.com>
2025-09-18 17:02:12 +0900

    public 폴더 경로 수정

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import './App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 //import Detail from './extra/Detail';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
