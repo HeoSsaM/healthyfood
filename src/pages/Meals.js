681f75c739fc5a7f00a2ffa665c8d53bb51345ac
허쌤 <withmyload@gmail.com>
2025-09-18 17:24:42 +0900

    리액트 깃 배포

--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-//import RecomCard from '../component/MealCard';
 import MealCard from '../component/MealCard';
 
 const Meals = () => {
@@ -7,7 +6,7 @@ const Meals = () => {
   const [showAll, setShowAll] = useState(false);//더보기 버튼 클릭 상태에 따라 true/false
 
   useEffect(()=>{
-    fetch('/db.json')
+    fetch(`${process.env.PUBLIC_URL}/db.json`)
       .then(r=>r.json()) //json 형식으로 데이터 변환
       .then(data => setAll(data.meals || [])) //json에서 meals 배열만 꺼내서 상태에 저장
       .catch(console.error);
